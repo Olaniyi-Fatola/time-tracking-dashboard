1f014e43cdb5d352e978828846e0e9214a00d083
Olaniyi-Fatola <olhaniyi08@gmail.com>
2025-09-10 08:04:39 -0400

    updated

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-const Daily = document.getElementById("#daily");
-const Weekly = document.getElementById("#weekly");
-const Monthly = document.getElementById("#monthly");
-const button = document.getElementById("period");
+const Daily = document.getElementById("daily");
+const Weekly = document.getElementById('weekly')
+const Monthly = document.getElementById("monthly");
+let clickCount = 0;
 
  
-
-
-/*Monthly.onclick = function(){
-    if(){
-        // monthly time track dashboard appear
-        
-        monthly();
-    }
-    else{
-        // it disappears when other time is clicked
-        
+//This show the daily dashboard also reverting back to the weekly dashbaord
+Daily.onclick = function(){
+        boxes.className = "visible";
+        boxes2.className = "";
+        boxesMonthly.className = "";
         daily();
-    }
-}*/
+}
+//Refresh the page to avoid increment in the daily() for loop
+Daily.addEventListener('click', () => {
+        clickCount++; // Increment the counter on each click
 
-
+        if (clickCount === 2) {
+            location.reload(); // Refresh the page on the second click
+        }
+    });
 async function daily() {
     const titles = await getdata();
 
@@ -29,82 +28,76 @@ async function daily() {
     for(const title of titles){
 
         titleCard.innerHTML += `
-                    <div id="${title.title}">
-                    <img class="icons" src="${title.image}" alt="work icon">
-                        <div id="cards">
-                            <div class="subject" >
-                                <div class="desktopdot">
-                                    <h4>${title.title}</h4>
-                                    <img class="dots" src="${title.ellipsis}" alt="ellipsis icon">
-                                </div>
-                                <h5>${title.timeframes.daily.current}hrs</h5>
-                            </div>
-                            <div class="ellipsis">
-                                <img class="threedots" src="${title.ellipsis}" alt="ellipsis icon">
-                                <p>Last Week-${title.timeframes.daily.previous}hrs</p>
-                            </div>
-                        </div>
+        <div id="${title.title}">
+        <img class="icons" src="${title.image}" alt="work icon">
+            <div id="cards">
+                <div class="subject" >
+                    <div class="desktopdot">
+                        <h4>${title.title}</h4>
+                        <img class="dots" src="${title.ellipsis}" alt="ellipsis icon">
                     </div>
-            `;
+                    <h5>${title.timeframes.daily.current}hrs</h5>
+                </div>
+                <div class="ellipsis">
+                    <img class="threedots" src="${title.ellipsis}" alt="ellipsis icon">
+                    <p>Yesterday-${title.timeframes.daily.previous}hrs</p>
+                </div>
+            </div>
+        </div>
+        `;
+    } 
+}  
+
+//This show the monthly dashboard also reverting back to the weekly dashbaord
+Monthly.onclick = function(){
+    boxes.className = "";
+    boxes2.className = "";
+    boxesMonthly.className = "visible";
+    monthly();
     }
-}
 
-async function weekly(data) {
-    const titles = await getdata();
+//Refresh the page to avoid increment in the monthly() for loop
+Monthly.addEventListener('click', () => {
+    clickCount++; // Increment the counter on each click
 
-    
-    const titleCard = document.querySelector('#boxes');
-
-    for(const title of titles){
-
-        titleCard.innerHTML += `
-                    <div id="${title.title}">
-                    <img class="icons" src="${title.image}" alt="work icon">
-                        <div id="cards">
-                            <div class="subject" >
-                                <div class="desktopdot">
-                                    <h4>${title.title}</h4>
-                                    <img class="dots" src="${title.ellipsis}" alt="ellipsis icon">
-                                </div>
-                                <h5>${title.timeframes.weekly.current}hrs</h5>
-                            </div>
-                            <div class="ellipsis">
-                                <img class="threedots" src="${title.ellipsis}" alt="ellipsis icon">
-                                <p>Last Week-${title.timeframes.weekly.previous}hrs</p>
-                            </div>
-                        </div>
-                    </div>
-            `;
+    if (clickCount === 2) {
+        location.reload(); // Refresh the page on the second click
     }
-}
-
+});
 async function monthly() {
     const titles = await getdata();
 
     
-    const titleCard = document.querySelector('#boxes');
+    const titleCard = document.querySelector('#boxesMonthly');
 
     for(const title of titles){
 
         titleCard.innerHTML += `
-                    <div id="${title.title}">
-                    <img class="icons" src="${title.image}" alt="work icon">
-                        <div id="cards">
-                            <div class="subject" >
-                                <div class="desktopdot">
-                                    <h4>${title.title}</h4>
-                                    <img class="dots" src="${title.ellipsis}" alt="ellipsis icon">
-                                </div>
-                                <h5>${title.timeframes.monthly.current}hrs</h5>
-                            </div>
-                            <div class="ellipsis">
-                                <img class="threedots" src="${title.ellipsis}" alt="ellipsis icon">
-                                <p>Last Week-${title.timeframes.monthly.previous}hrs</p>
-                            </div>
-                        </div>
+        <div id="${title.title}">
+        <img class="icons" src="${title.image}" alt="work icon">
+            <div id="cards">
+                <div class="subject" >
+                    <div class="desktopdot">
+                        <h4>${title.title}</h4>
+                        <img class="dots" src="${title.ellipsis}" alt="ellipsis icon">
                     </div>
-            `;
+                    <h5>${title.timeframes.monthly.current}hrs</h5>
+                </div>
+                <div class="ellipsis">
+                    <img class="threedots" src="${title.ellipsis}" alt="ellipsis icon">
+                    <p>Last Month-${title.timeframes.monthly.previous}hrs</p>
+                </div>
+            </div>
+        </div>
+        `;
     }
+}
+        
+//This show the weekly dashboard
+Weekly.onclick = function(){
+    boxes.className = "";
+    boxes2.className = "visible";
+    boxesMonthly.className = "";
 }
 
 
@@ -114,25 +107,3 @@ async function getdata() {
 
     return data;
 }
-
-
-async function main(){
-    const abc = await daily();
-    const bcd = await weekly();
-    const efg = await monthly();
-
-    if(button === Weekly){
-        // weekly time track dashboard appear
-        
-        abc;
-    }
-    else if(button === Daily){
-        // it disappears when other time is clicked
-        bcd;
-    }
-    else {
-        efg;
-    }
-}
-
-button.addEventListener('click', main);
